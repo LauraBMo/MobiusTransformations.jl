a5c79ca20544a8ebca5f47dc7ed2869f6cc2f1f7
LauBMo <laurea987@gmail.com>
2026-09-08 00:06:00 +0200

    Add MöbiusTransformation type
    A Möbius map is stored by its four coefficients a, b, c, d, with an
    outer constructor that promotes mixed types.

--- src/MobiusTransformations.jl
+++ src/MobiusTransformations.jl
@@ -13,4 +13,24 @@ function set_infinity(infinity)
     INF[] = infinity
 end
 
+# Möbius transformation
+"""
+    MöbiusTransformation{T}
+
+Represents a Möbius transformation of the form:
+f(z) = (a*z + b) / (c*z + d)
+
+# Fields
+- `a::T, b::T, c::T, d::T`: Coefficients of the transformation
+"""
+struct MöbiusTransformation{T} <: Function
+    a::T
+    b::T
+    c::T
+    d::T
+end
+
+# Dealing with diferent types
+MöbiusTransformation(a, b, c, d) = MöbiusTransformation(promote(a, b, c, d)...)
+
 end # of module MobiusTransformations.
